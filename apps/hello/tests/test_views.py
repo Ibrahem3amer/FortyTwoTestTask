63337d157665330a7a0ce7143f28cc57ffdb593a
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-07-19 16:44:35 +0200

    ticket#3 Middleware tests faild.

--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -29,3 +29,63 @@ class user_vists_pages(TestCase):
         # Assert test
         self.assertEqual(request.status_code, 200)
         self.assertTemplateUsed(request, 'requests.html')
+
+
+class RequestTest(TestCase):
+
+    def test_add_new_request_through_view(self):
+        """Tests that middleware saves incoming request."""
+        
+        # Setup test
+        self.client.get(reverse('visitor_homepage'))
+
+        # Exercise test
+        requests_in_db = Request.objects.all().count()
+
+        # Assert test
+        self.assertTrue(requests_in_db > 0)
+
+
+    def test_add_more_than_one_request(self):
+        """Tests that middleware saves incoming multiple request."""
+
+        # Setup test
+        for i in range(5):
+            self.client.get(reverse('visitor_homepage'))
+
+        # Exercise test
+        requests_in_db = Request.objects.all().count()
+
+        # Assert test
+        self.assertTrue(requests_in_db > 4)
+
+
+    def test_unread_requests(self):
+        """Tests that middleware increments unread requests."""
+
+        # Setup test
+        self.client.get(reverse('visitor_homepage'))
+
+        # Exercise test
+        requests_in_db = Request.objects.all().count()
+        n = RequestHandler.get_unread_requests()
+
+        # Assert test
+        self.assertTrue(requests_in_db > 0)
+        self.assertTrue(n > 0)
+
+
+    def test_read_the_unread(self):
+        """Tests that middleware updated unread requests when user reads them."""
+        
+        # Setup test
+        self.client.get(reverse('visitor_homepage'))
+
+        # Exercise test
+        n = RequestHandler.get_unread_requests()
+        self.client.get(reverse('request_page'))
+        new_n = RequestHandler.get_unread_requests()
+
+        # Assert test
+        self.assertTrue(new_n != n)
+        self.assertTrue(n > new_n)
