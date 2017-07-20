59bb2731a815c6a45f0f149216f4c75f497444b9
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-07-20 16:25:44 +0200

    ticket#5 ModelForm tests added.

---
+++ apps/hello/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from hello.models import Person
+from hello.validators import EditPersonValidator
+
+class EditPersonForm(forms.ModelForm):
+
+    class Meta:
+        model       = Person
+        fields      = '__all__'
+    
+    def clean(self):
+        
+        super(EditPersonForm, self).clean()
+        cleaned_data = self.cleaned_data
+
+        # Validates that birthdate is in normal range.
+        bdate_status = EditPersonValidator.validate_birth_date(cleaned_data['birth_date'])
+        if bdate_status != 1:
+            raise ValidationError('Please enter a valid year.')
+
+        
---
+++ apps/hello/tests/test_forms.py
@@ -0,0 +1,89 @@
+from hello.models import Person
+from hello.forms import EditPersonForm
+from django.test import TestCase
+
+
+class EditPersonFormTest(TestCase):
+
+
+    def test_initiate_basic_form(self):
+        """Tests the creation of form with basic valid data."""
+        
+        # Setup test
+        data  = {
+            'first_name': 'Ibrahem',
+            'sur_name': 'Amer',
+            'birth_date': '1995-5-5',
+            'bio': 'bla bal bla.',
+            'contacts': "email: sdasd /nskype: ebrahem3amer",
+            'photo': 'N/A',
+        }
+
+        # Exercise test
+        form = EditPersonForm(data=data)
+
+        # Assert test
+        self.assertTrue(form.is_valid())
+
+
+    def test_edit_fname_with_numbers(self):
+        """Tests creation with invalid fname."""
+        
+        # Setup test
+        data  = {
+            'first_name': '123456ibrahem',
+            'sur_name': 'Amer',
+            'birth_date': '1995-5-5',
+            'bio': 'bla bal bla.',
+            'contacts': "email: sdasd /nskype: ebrahem3amer",
+            'photo': 'N/A',
+        }
+        
+
+        # Exercise test
+        form = EditPersonForm(data=data)
+
+        # Assert test
+        self.assertFalse(form.is_valid())
+
+
+    def test_edit_sname_with_numbers(self):
+        """Tests creation with invalid sname."""
+
+        # Setup test
+        data  = {
+            'first_name': 'Ibrahem',
+            'sur_name': '1adel',
+            'birth_date': '1995-5-5',
+            'bio': 'bla bal bla.',
+            'contacts': "email: sdasd /nskype: ebrahem3amer",
+            'photo': 'N/A',
+        }
+        
+
+        # Exercise test
+        form = EditPersonForm(data=data)
+
+        # Assert test
+        self.assertFalse(form.is_valid())
+
+
+    def test_edit_birth_date_with_date_less_than_10_years_ago(self):
+        """Tests creation with invalid birthday."""
+
+        # Setup test
+        data  = {
+            'first_name': 'Ibrahem',
+            'sur_name': 'Amer',
+            'birth_date': '17-12-2010',
+            'bio': 'Relentless programmer.',
+            'contacts': "email: sdasd /nskype: ebrahem3amer",
+            'photo': 'N/A',
+        }
+        
+
+        # Exercise test
+        form = EditPersonForm(data=data)
+
+        # Assert test
+        self.assertFalse(form.is_valid())
